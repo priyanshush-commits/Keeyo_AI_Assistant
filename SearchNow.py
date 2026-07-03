371022f846831996fdc4f40fc877cd305723608c
Priyanshu Kumar <shahpriyanshu024@gmail.com>
2026-07-03 23:50:04 +0545

    Update SearchNow.py

--- SearchNow.py
+++ SearchNow.py
@@ -1,1 +1,48 @@
+import speech_recognition
+import pyttsx3
+import pywhatkit
+import wikipedia
 
+def takeCommand():
+    r = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.energy_threshold = 300
+        audio = r.listen(source,0,4)
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"You Said: {query}\n")
+    except Exception as e:
+        print("Say that again please...")  
+        return "None"
+    return query
+
+query = takeCommand().lower()
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice",voices[0].id)
+print(voices[0])
+engine.setProperty("rate",180)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def searchGoogle(query):
+  if "google" in query:
+    import wikipedia as googleScrap
+    query = query.replace("keeyo","")
+     query = query.replace("google search","")
+     query = query.replace("google","")
+     speak("This is what I found on google")
+
+    try:
+        pywhatkit.search(query)
+        result = googleScrap.summary(query,1)
+        speak(result)
+      
+    
+    
